21915c4b7c6ff4fd1de538fbc27f73c37e1957ec
LorenzoGiacchetta <lorenzogiacchetta1005@gmail.com>
2022-10-04 18:01:39 -0300

    -agregamos los blazor componets que no se ven en la solucion (area identity). -cargamos un usuario y roles asignacion de del us al rol -falta: -agregar autorizaciones a componentes (https://learn.microsoft.com/es-es/aspnet/core/blazor/security/?view=aspnetcore-5.0). -agregrar roles us -agregar servicios

---
+++ Blazor/DDR/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DDR.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DDR.Areas.Identity.IdentityHostingStartup))]
+namespace DDR.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
