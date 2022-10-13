18ab7f589e297692038aeb5e82a4dd99eb6bfed8
fefo7 <INFO-4@INFO2-PC10>
2022-10-13 12:25:55 -0300

    Creacion del rol Admin,Impkementacion del rol en el starup y restriccion de la pagina de cargar patente para que solo lo vea si sos admin.
    ¿que falta hacer?
    agregar nuevo rol de UserDefault
    retringir sus permisos
    crear pagina web para que se puedan cargar patentes desde la base de datos.
    preview de la base de datos
    restriccion para la vista de solo admin

--- Blazor/DDR/Program.cs
+++ Blazor/DDR/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -7,15 +9,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace DDR
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            
             CreateHostBuilder(args).Build().Run();
-        }
 
+        }
+        
+            
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
@@ -29,5 +35,6 @@ namespace DDR
                     //});
                     webBuilder.UseStartup<Startup>();
                 });
+
     }
 }
