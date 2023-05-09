93195a63bf1a83e47b0a0ac0cf0938cc5c81b244
Federico Jose Isla <federicoisla1@gmail.com>
2023-05-09 10:36:08 -0300

    Mejoras del reconocimiento: -Establece tamaño iguales para todas las imagenes -Creacion de kernelNew para poder desefocar de una mejor manera las imagenes -Restricciones de ancho y alto para detectar SOLO patentes de esas proporciones -Normalizacion de caracteres a la hora de lectura de digitos Segun las estadisticas, el reconocimiento en base a 22 imagenes, detecta 11 y las otras no las lee pero recorta sobre el rectangulo de las patentes
    apeleno :)

--- Python/Reconocimiento.py
+++ Python/Reconocimiento.py
@@ -5,7 +5,7 @@ import base64
 from flask import *
 from config import config
 
-
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 class Reconocimiento:
     def obtenerPlaca(self, img):
         (canny,gray) = self.PintarImagen(img)
@@ -13,13 +13,26 @@ class Reconocimiento:
         placa = self.PorcionReconocimiento(cnts, gray)
         return self.lecDigitos(placa)
 
-    def PintarImagen(image, blurP =(3, 3), cannyP = [150, 200], iterations=1):
-        cv2.imshow("Pintar",image)
+    def PintarImagen(image, gausP =(3, 3), cannyP = [100, 200], iterations=1):
+        # Estblece una resolucion fija a la imagen
+        img = cv2.resize(image, (500,500))
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        #(3,5)--> 11 de 22 patentes
+        # nucleo para poder desenfocar y reducir ruido
+        kernelNew = np.ones((3,5), np.float32)/15
+        gray = cv2.filter2D(gray, -1,kernelNew)
+        cv2.imshow("kerne", gray)
         cv2.waitKey(0)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        gray = cv2.blur(gray, blurP)
-        canny = cv2.Canny(gray, cannyP[0], cannyP[1])
+        #gaussiana = cv2.GaussianBlur(gray, gausP, 10)
+        binary = cv2.threshold(gray, 170, 255, cv2.THRESH_BINARY_INV)[1]
+        cv2.imshow("binary", binary)
+        cv2.waitKey(0)
+
+        canny = cv2.Canny(binary, cannyP[0], cannyP[1])
         canny = cv2.dilate(canny, None, iterations)
+        cv2.imshow("canny", canny)
+        cv2.waitKey(0)
+
         return (canny,gray)
 
     def Contornos(canny, image):
@@ -27,21 +40,26 @@ class Reconocimiento:
         cv2.drawContours(image, cnts, -1, (0, 255, 0), 2)
         return  cnts;
         # CALCULA EL AREA DE ESA PORCION DONDE SE ENCUENTRA LA PATENTE
-    def PorcionReconocimiento (cnts, gray):
+    def PorcionReconocimiento (cnts, gray,min_w=80, max_w=110, min_h=25, max_h=52, ratio=2.5):
+
         for c in cnts:
             area = cv2.contourArea(c)
             x, y, w, h = cv2.boundingRect(c)
             epsilon = 0.07 * cv2.arcLength(c, True)
             approx = cv2.approxPolyDP(c, epsilon, True)
+            aspect_ratio = float(w) / h
             # RECONOCEMOS EL AREA Y LAS ARISTAS DE LA IMAGEN
-            if len(approx) == 4 and 5000 < area < 70000:
-                # print('area=', area)
-                # cv2.drawContours(image, [approx], 0, (0, 255, 0), 3)
-                aspect_ratio = float(w) / h
-                if aspect_ratio > 1.5:
+            if len(approx) == 4 and 1000 < area < 10000 and (max_w > w > min_w) and (max_h > h > min_h):
+                if aspect_ratio > ratio:
                     placa = gray[y:y + h, x:x + w]
-                    return placa
-    def lecDigitos(placa, textAux = ""):
+                    cv2.imshow("placa", placa)
+                    cv2.waitKey(0)
+                    return
+    def lecDigitos(BinaryPlaca, textAux = "", psm=7):
         # EXTRAE EL TEXTO Y LO MUESTRA POR CONSOLA
-        text = textAux + pytesseract.image_to_string(placa, config='--psm 7')
+        # NORMALIZACION DE CARACTERES
+        alphanumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        options = "-c tessedit_char_whitelist={}".format(alphanumeric)
+        options += " --psm {}".format(psm)
+        text = textAux + pytesseract.image_to_string(BinaryPlaca, config=options)
         return print('PLACA: ', text)
