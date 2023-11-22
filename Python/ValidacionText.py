362e64393f2d9f7188a9c15f1971905185975c07
Federico Jose Isla <federicoisla1@gmail.com>
2023-11-22 17:23:15 -0300

    cambio del endpoint de validacion 2

--- Python/ValidacionText.py
+++ Python/ValidacionText.py
@@ -1,11 +1,11 @@
 import re
 class Validacion:
     def LimpiarTexto(cadena):
-        caracteres_especiales = re.compile(r"[^a-zA-Z0-9]")
-        cadena = caracteres_especiales.sub("", cadena)
-        if len(cadena) not in [6, 7]:
+        patente = re.compile(r"^[a-zA-Z]{3}\d{3}$|^[a-zA-Z]{2}\d{3}[a-zA-Z]{2}$")
+        if patente.fullmatch(cadena):
+            return 1
+        else:
             return 0
-        return 1
     def LimpiarTextoReco(cadena):
         caracteres_especiales = re.compile(r"[^a-zA-Z0-9]")
         cadena = caracteres_especiales.sub("", cadena)
